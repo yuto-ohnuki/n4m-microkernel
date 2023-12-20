d0ee8a03704ebb2da031948b3a8badead4494dbb
yuto_ohnuki <yuto.personal92@gmail.com>
2023-12-20 13:50:40 +0900

    upload exception

--- kernel.h
+++ kernel.h
@@ -1,10 +1,61 @@
 #pragma once
+#include "common.h"
+
+// 例外処理で使うマクロ
+struct trap_frame {
+    uint32_t ra;
+    uint32_t gp;
+    uint32_t tp;
+    uint32_t t0;
+    uint32_t t1;
+    uint32_t t2;
+    uint32_t t3;
+    uint32_t t4;
+    uint32_t t5;
+    uint32_t t6;
+    uint32_t a0;
+    uint32_t a1;
+    uint32_t a2;
+    uint32_t a3;
+    uint32_t a4;
+    uint32_t a5;
+    uint32_t a6;
+    uint32_t a7;
+    uint32_t s0;
+    uint32_t s1;
+    uint32_t s2;
+    uint32_t s3;
+    uint32_t s4;
+    uint32_t s5;
+    uint32_t s6;
+    uint32_t s7;
+    uint32_t s8;
+    uint32_t s9;
+    uint32_t s10;
+    uint32_t s11;
+    uint32_t sp;
+} __attribute__((packed));
 
 // SBI の処理結果を返すための構造体
 struct sbiret {
     long error;
     long value;
 };
+
+// CSR レジスタの読み書きを行うためのマクロ
+#define READ_CSR(reg)                                                          \
+    ({                                                                         \
+        unsigned long __tmp;                                                   \
+        __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp));                  \
+        __tmp;                                                                 \
+    })
+
+// CSR レジスタの読み書きを行うためのマクロ
+#define WRITE_CSR(reg, value)                                                  \
+    do {                                                                       \
+        uint32_t __tmp = (value);                                              \
+        __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                \
+    } while (0)
 
 // kernel panic
 // **_VA_ARGS__: コンパイラ拡張機能。## により、可変長引数が空の時に直前の , を削除
