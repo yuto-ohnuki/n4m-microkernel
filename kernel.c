98dac822c3c23626b50cf40cb2703502d217db2d
yuto_ohnuki <yuto.personal92@gmail.com>
2023-12-21 12:32:40 +0900

    update memory-allocation

--- kernel.c
+++ kernel.c
@@ -7,6 +7,7 @@ typedef uint32_t size_t;
 
 // リンカスクリプト内で定義されている各シンボル
 extern char __bss[], __bss_end[], __stack_top[];
+extern char __free_ram[], __free_ram_end[];
 
 // OpenSBI を呼び出す関数
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid){
@@ -142,10 +143,30 @@ void kernel_entry(void){
     );
 }
 
+// n ページ分のメモリを動的に割り当てる関数
+paddr_t alloc_pages(uint32_t n){
+    static paddr_t next_paddr = (paddr_t) __free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    // __free_ram_end を超えるアドレスへと割り当てようとした場合にはカーネルパニック
+    if (next_paddr > (paddr_t) __free_ram_end){
+        PANIC("out of memory");
+    }
+    
+    // 割り当てたメモリ領域のゼロ初期化
+    memset((void *) paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
+
 void kernel_main(void) {
     // printf("\n\nHello %s\n", "World!");
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-    WRITE_CSR(stvec, (uint32_t) kernel_entry);
-    __asm__ __volatile__("unimp"); // 架空の命令・無効
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("alloc_pages test: paddr0=%x\n", paddr0);
+    printf("alloc_pages test: paddr1=%x\n", paddr1);
+
+    PANIC("booted!");
 }
